e7c32edea02a0c0338cbcfe7905f11525c651441
Tair Assimov <tair@assimov.net>
2009-11-17 02:16:36 +0400

    Adding support for X Axis steps. Works with XAxisLabels and plain values.
    Example usage:
    
    x = XAxis.new
    x.set_labels(x_labels) OR x.set_labels(Array)
    
    \# One week span
    x.set_steps(7)

--- lib/open_flash_chart/x_axis.rb
+++ lib/open_flash_chart/x_axis.rb
@@ -4,12 +4,20 @@ module OpenFlashChart
     def set_3d(v)
       @threed = v
     end
+    
     # for some reason the json that needs to be produced is like this:
-    # "x_axis": { "offset": false, "labels": { "labels": [ "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" ] } }
-    # note the "labels":{"labels": ....}
+    # "x_axis": { "steps": X, "offset": false, "labels": { "labels": [ "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" ], "steps": X } }
+    # note the "labels":{"labels": ....} and "steps":{"steps": .... }
+    
     def set_labels(labels)
       @labels = labels
       @labels = {:labels => labels} unless labels.is_a?(XAxisLabels)
+    end
+    
+    def set_steps(steps = nil)
+      return if steps.nil?
+      @steps = steps
+      @labels.is_a?(XAxisLabels) ? @labels.set_steps(steps) : @labels.merge!({:steps => steps})
     end
 
     alias_method :labels=, :set_labels
--- lib/open_flash_chart/x_axis_labels.rb
+++ lib/open_flash_chart/x_axis_labels.rb
@@ -1,9 +1,15 @@
 module OpenFlashChart
 
   class XAxisLabels < Base
+    
+    def set_steps(steps = 1)
+      @steps = steps
+    end
+    
     def set_vertical
       @rotate = "vertical"
     end
+    
   end
 
 end
